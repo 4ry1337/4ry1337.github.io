f3cc97fb160291e2277a5cd140f1f3a4416d845f
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-05-23 01:09:50 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use actix_web::{App, HttpServer};
-
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new())
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+fn main() {
+    println!("Hello, world!");
 }
