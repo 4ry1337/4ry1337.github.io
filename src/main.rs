fcb28a4081c38188dbd4086916164927f2712b0e
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-06-01 20:54:56 +0500

    remove tracing

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@ use ary1337::{
     config::get_config,
     error::Error,
     routes::{Routes, about::about, home::home},
-    tracing::{get_subscriber, init_subscriber},
 };
-use tracing::info;
 
 fn main() -> Result<(), Error> {
-    let subscriber = get_subscriber("ary1337".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
-    info!("Starting ary1337");
     let config = get_config().expect("Failed to read configuration.");
     let routes = Routes::new(config)
-        .route("/home", home())
+        .route("/", home())
         .route("/about", about());
 
     routes.build()?;
-    info!("Finished");
     Ok(())
 }
