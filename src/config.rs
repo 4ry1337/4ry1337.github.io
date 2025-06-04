b89a75f34e4689463bcd567d4a730b3cdbfd557e
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-06-05 02:14:28 +0500

    migrating to scss
    a
    
    a

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,8 @@
 use serde::Deserialize;
-use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Settings {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub port: u16,
-    pub host: String,
+    pub base_url: String,
     pub output: String,
 }
 
